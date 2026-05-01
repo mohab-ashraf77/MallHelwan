e6cdab4e6cb0985d99658067ed282c1b7a8c5b71
samirabeer281-create <samirabeer281@gmail.com>
2026-05-01 15:03:30 +0300

    Add files via upload

---
+++ entertainment.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".card");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    buttons.forEach(function(btn) {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    const filter = button.getAttribute("data-filter");
+
+    cards.forEach(function(card) {
+      const category = card.getAttribute("data-category");
+
+      if (filter === "all" || filter === category) {
+        card.classList.remove("hide");
+      } else {
+        card.classList.add("hide");
+      }
+    });
+  });
+});
